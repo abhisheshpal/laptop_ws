24142c39a75e01f533127da293e6882927ec5216
Vignesh <Vignesh-Raja.Ponnambalam@eleves.ec-nantes.fr>
2018-03-29 20:26:17 +0200

    Add files via upload

--- src/thorvald_2d_nav/include/thorvald_2d_nav/test_row_transition.h
+++ src/thorvald_2d_nav/include/thorvald_2d_nav/test_row_transition.h
@@ -36,17 +36,14 @@ sensor_msgs::LaserScan scan_msg_main;
 size_t num_ranges;
 bool goal_found = false;
 double sum_x_1 = 0, sum_y_1 = 0;
-int k = 0, q = 0, unfit = 0, unfit_1 = 0;
-double current_range_1, current_range_2, current_range_3, angle_1, angle_2, angle_3;
+int k = 0, q = 0, n = 0, unfit = 0, unfit_1 = 0, unfit_2 = 0;
+double current_range_1, current_range_2, current_range_3, current_range_4, angle_1, angle_2, angle_3, angle_4;
 int row_transit_mode = 0;
 double nearest_pole_x, nearest_pole_y, next_nearest_pole_x, next_nearest_pole_y, farthest_pole_x, farthest_pole_y;
 
 //Pole Markers
-visualization_msgs::Marker marker_1;
-visualization_msgs::Marker marker_2;
-visualization_msgs::Marker marker_3;
-visualization_msgs::Marker marker_goal_1;
-visualization_msgs::Marker marker_goal_2;
+visualization_msgs::Marker marker_1, marker_2, marker_3, marker_4;
+visualization_msgs::Marker marker_goal_1, marker_goal_2;
 
 //dummy variables
 double linear_velocity = 0.5, angular_velocity = 0.0;
