7d271e34553e5eee6f0e14c55431870bf8a60249
Vignesh <vignesh.raja199@gmail.com>
2018-04-14 12:13:45 +0200

    Update fastSLAM.cpp

--- src/thorvald_2d_nav/src/fastSLAM.cpp
+++ src/thorvald_2d_nav/src/fastSLAM.cpp
@@ -269,6 +269,19 @@ for (int i = 1; i <= numParticles; i++){
 
 }
 
+void resample(){
+N= length(particles);
+w= zeros(1,N);
+for i=1:N, w(i)= particles(i).w; end
+ws= sum(w); w= w/ws;
+for i=1:N, particles(i).w= particles(i).w / ws; end
+
+[keep, Neff] = stratified_resample(w);
+if Neff < Nmin & doresample==1
+    particles= particles(keep);
+for i=1:N, particles(i).w= 1/N; end
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "fastSLAM");
